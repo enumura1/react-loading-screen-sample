e6944d52232efe9a1c77b6038986276007f7d9db
enumura1 <akirayuto0718@gmail.com>
2024-01-05 15:34:10 +0900

    update:components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+
+// 外部関数のインポート
+import LoadingScreen from './Loading';
+import LoadedScreen from './Loaded';
+
+// 外部ファイルのインポート
+import './App.css';
 
 export default function App() {
+  const [loading, setLoading] =useState(true);
+
+  useEffect(() => {
+      const timeoutId = setTimeout(() => {
+        setLoading(false)
+      }, 5000);
+
+      // useEffectがクリーンアップされたときにタイマーをクリア
+      return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div>hogehogehoge</div>
-  )
+    <div >
+      {loading ? (<LoadingScreen/>):(<LoadedScreen/>)}
+    </div>
+  );
 }
+
---
+++ src/Loaded.tsx
@@ -0,0 +1,10 @@
+
+function LoadedScreen() {
+  return (
+    <div className='LoadedMain'>
+      <h1>ロード後の画面</h1>
+    </div>
+  )
+}
+
+export default LoadedScreen;
